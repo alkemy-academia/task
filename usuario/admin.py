a896ed8762fc66c47da81baa8e9dcf44a33098f2
cgallardo <cecigallardo@gmail.com>
2024-04-22 10:10:27 -0300

    Agregar admin de usaurio

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from usuario.models import Usuario
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    search_fields = ['username']
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        # Agregar el campo dni al fieldset 'Personal info'
+        if obj:
+            fieldsets[1][1]['fields'] += ('documento_identidad',)
+        return fieldsets
